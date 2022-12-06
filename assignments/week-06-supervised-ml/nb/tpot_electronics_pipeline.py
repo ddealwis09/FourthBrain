1b1f0c1ff7fbdfe116b3b4ad4348ec2d89ca3b51
ddealwis09 <dinushdealwis@gmail.com>
2022-12-06 13:08:18 -0500

    comment out widget

--- assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
+++ assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import FastICA
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.svm import LinearSVC
+from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -14,8 +16,11 @@ training_features, testing_features, training_target, testing_target = \
 
 # Average CV score on the training set was: 1.0
 exported_pipeline = make_pipeline(
-    FastICA(tol=0.30000000000000004),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=2, min_samples_split=5, n_estimators=100)
+    make_union(
+        FunctionTransformer(copy),
+        FunctionTransformer(copy)
+    ),
+    LinearSVC(C=20.0, dual=False, loss="squared_hinge", penalty="l1", tol=0.01)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
